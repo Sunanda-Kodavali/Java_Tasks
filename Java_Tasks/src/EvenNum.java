aca57619e8549bf9777d01172eb056b407cdd212
SunandaKodavali <sunanda.kodavali@gmail.com>
2024-03-20 09:58:30 +0100

    added evennum

--- Java_Tasks/src/EvenNum.java
+++ Java_Tasks/src/EvenNum.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
-public class Main {
+public class EvenNum {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            System.out.print(1);
+        for (int i = 2; i <= n; i+=2) {
+            System.out.print(i+" ");
         }
-
     }
 }
