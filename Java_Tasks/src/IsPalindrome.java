3fd10c24407b56d62349cbfc0d29879e52b0e5df
SunandaKodavali <sunanda.kodavali@gmail.com>
2024-05-23 09:47:25 +0200

    changed pallindrome

--- Java_Tasks/src/IsPalindrome.java
+++ Java_Tasks/src/IsPalindrome.java
@@ -2,19 +2,19 @@
 
 public class IsPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome("amanaplanacanalpanama"));
     }
-    public static boolean isPalindrome(int x) {
-        int y = x;
-        int reverse = 0;
-        if (x < 0) {
-            return false;
+    public static boolean isPalindrome(String s) {
+
+        String sl = s.toLowerCase().replaceAll("[^a-zA-Z0-9_-]", "");
+
+        for (int i = 0; i < sl.length() / 2; i++) {
+                if(sl.charAt(i) != sl.charAt(sl.length()-i-1)){
+                    return false;
+
+            }
         }
-        while (x != 0) {
-            int remainder = x % 10;
-            reverse = reverse * 10 + remainder;
-            x = x / 10;
-        }
-        return y == reverse;
+        return true;
+
     }
 }
